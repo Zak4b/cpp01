a761f1b876c14425bdfcd1430d6e164cee34f88d
Alain <alain.sn.66@gmail.com>
2025-03-25 16:17:08 +0100

    main

--- ex06/main.cpp
+++ ex06/main.cpp
@@ -7,8 +7,10 @@ int	main(int argc, char ** argv)
 	Harl	test;
 
 	if (argc != 2)
+	{
 		std::cout << "Usage : " << argv[0] << " <LEVEL>" << std::endl;
-	else
-		test.complain(argv[1]);
+		return (1);
+	}
+	test.complain(argv[1]);
 	return (0);
 }
