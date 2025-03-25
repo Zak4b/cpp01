13b33863556b02054e8a6fbb289c8f5c919cc667
Alain <alain.sn.66@gmail.com>
2025-03-25 15:33:59 +0100

    ex05

---
+++ ex05/Harl.cpp
@@ -0,0 +1,56 @@
+#include "Harl.hpp"
+#include <iostream>
+
+Harl::Harl(/* args */)
+{
+}
+
+Harl::~Harl()
+{
+}
+
+void Harl::print(std::string str)
+{
+	std::cout << str << std::endl;
+}
+
+void Harl::debug( void )
+{
+	this->print("I love having extra bacon for my 7XL-double-cheese-triple-pickle-specialketchup burger. I really do!");
+}
+void Harl::info( void )
+{
+	this->print("I cannot believe adding extra bacon costs more money. You didn't put enough bacon in my burger! If you did, I wouldn't be asking for more!");
+}
+void Harl::warning( void )
+{
+	this->print("I think I deserve to have some extra bacon for free. I've been coming for years, whereas you started working here just last month.");
+}
+void Harl::error( void )
+{
+	this->print("This is unacceptable! I want to speak to the manager now.");
+}
+
+void Harl::complain( std::string level )
+{
+	
+	unsigned int	i = 0;
+	std::string levels[] = {
+		"DEBUG",
+		"INFO",
+		"WARNING",
+		"ERROR"
+	}; 
+	void (Harl::*f[])() = {
+		&Harl::debug,
+		&Harl::info,
+		&Harl::warning,
+		&Harl::error
+	};
+	while (i < sizeof(levels) / sizeof(levels[0]))
+	{
+		if (level == levels[i])
+			return (this->*f[i])();
+		i++;
+	}
+}
---
+++ ex05/main.cpp
@@ -0,0 +1,13 @@
+#include "Harl.hpp"
+#include <string>
+#include <iostream>
+
+int	main(void)
+{
+	Harl harlo;
+
+	harlo.complain("DEBUG");
+	harlo.complain("INFO");
+	harlo.complain("WARNING");
+	harlo.complain("ERROR");
+}
